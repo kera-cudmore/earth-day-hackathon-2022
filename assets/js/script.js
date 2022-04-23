febf244518f05efc2e6ca6c7adbc68c532f45fc4
JanelleG51 <janelle_macmillan@yahoo.co.uk>
2022-04-23 17:45:17 +0000

    feat: add fixed and mobile nav functionality

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+/*----Fixed nav scroll----*/
+    $('.nav-quiz-section').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('fixed');
+        } else {
+            $('nav').removeClass('fixed');
+        }
+    }, {
+        offset: '60px'
+    });
+
+/*----Mobile nav toggle----*/
+    $('.mobile-icon-js').click(function() {
+        var nav = $('.main-nav-js');
+        var icon = $('.mobile-icon-js i');
+
+        nav.slideToggle(200);
+
+        if (icon.hasClass('fa-bars')) {
+            icon.addClass('fa-xmark');
+            icon.removeClass('fa-bars');
+        } else {
+            icon.addClass('fa-bars');
+            icon.removeClass('fa-xmark');
+        }
+    });
+ 
+
+});
